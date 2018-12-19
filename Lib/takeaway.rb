3b99bc643beccd87407763d762c00b493282d3ad
NicGiles <n.r.giles1@gmail.com>
2018-12-19 16:09:25 +0000

    doubled menu and order within in order to isolate takeaway tests

--- Lib/takeaway.rb
+++ Lib/takeaway.rb
@@ -1,21 +1,20 @@
-require 'menu'
-require 'order'
+require_relative 'menu'
+require_relative 'order'
 
 class Takeaway
 attr_reader :menu, :order
 
-  def initialize
-@menu = Menu.new
-@order = Order.new
+  def initialize(menu = Menu.new, order = Order.new)
+    @menu = menu
+    @order = order
 end
 
 def show_menu
-menu.add_dishes
+  menu.options.each { |dish| puts "#{dish[:dish]}: £#{dish[:price]}"}
 end
 
-def make_order
-order.order
+def make_order(dish, quantity)
+  order.order(dish, quantity)
 end
-
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,21 +1,18 @@
-#
-# require 'takeaway'
-# require 'menu'
-# require 'order'
-#
-# describe Takeaway do
-#
-#   subject(:takeaway)     { described_class.new }
-#   # let(:choice)          { double :choice, :name => "Beef Onglet", :price => 6.50 }
-#
-#   describe '#show_menu' do
-#     it 'should show a list of dishes with prices' do
-#       expect(takeaway.show_menu).to be_an(Array)
-#       end
-#
-# #       it "allows customers to order" do
-# #       expect (takeaway.make_order(choice, 4).to eq('4 x haggis added')
-# #
-# #
-# # end
-# end
+
+require 'takeaway'
+require 'menu'
+require 'order'
+
+describe Takeaway do
+
+let(:menu)  { double :menu, :options => [{ :dish => "Beef Onglet", :price => 6.50 }]  }
+let(:order)  { double :order }
+
+subject(:takeaway) { described_class.new(menu, order) }
+
+  describe '#show_menu' do
+    it 'should show a list of dishes with prices' do
+      expect { takeaway.show_menu }.to output("Beef Onglet: £6.5\n").to_stdout
+      end
+    end
+end
