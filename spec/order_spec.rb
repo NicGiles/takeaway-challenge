838765ab75c51ad6e6a52c130708bc8933b7f469
NicGiles <n.r.giles1@gmail.com>
2018-12-27 16:13:45 +0000

    Updated order spec to provide customer more information when they order'

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,7 +3,10 @@
 require 'order'
 
 describe Order do
-  subject(:customer_order)  { described_class.new }
+
+  let(:menu)  { double :menu, :options => {Curry: 4, Onglet: 7 } }
+
+subject(:customer_order)  { described_class.new }
 
   describe "#order" do
     it 'adds customer orders to the basket' do
@@ -13,9 +16,9 @@ describe Order do
 
   describe "#order_total" do
     it 'collates dishes customer has ordered' do
-      customer_order.order('Cheerios', 3)
-      customer_order.order('Beef Onglet', 3)
-      expect { customer_order.order_total }.to output("3 x Cheerios\n3 x Beef Onglet\n").to_stdout
+      customer_order.order('Onglet', 3)
+      expect { customer_order.order_total }.to output("\"3 x Onglet @ £7 : £21\"\nTotal : £21\"\n").to_stdout
+
     end
   end
 end
